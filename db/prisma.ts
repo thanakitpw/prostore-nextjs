0ada659fd6bd67d2ffa462a3cf1ec0873e8030b2
thanakitpw <thanakitpw@gmail.com>
2025-06-06 00:53:44 +0700

    Initial Deploy Fixed

--- db/prisma.ts
+++ db/prisma.ts
@@ -8,25 +8,25 @@ neonConfig.webSocketConstructor = ws;
 const connectionString = `${process.env.DATABASE_URL}`;
 
 // Creates a new connection pool using the provided connection string, allowing multiple concurrent connections.
-const pool = new Pool({ connectionString });
+// const pool = new Pool({ connectionString });
 
 // Instantiates the Prisma adapter using the Neon connection pool to handle the connection between Prisma and Neon.
-const adapter = new PrismaNeon(pool);
+const adapter = new PrismaNeon({ connectionString });
 
 // Extends the PrismaClient with a custom result transformer to convert the price and rating fields to strings.
 export const prisma = new PrismaClient({ adapter }).$extends({
-   result: {
-      product: {
-         price: {
-            compute(product) {
-               return product.price.toString();
-            },
-         },
-         rating: {
-            compute(product) {
-               return product.rating.toString();
-            },
-         },
+  result: {
+    product: {
+      price: {
+        compute(product) {
+          return product.price.toString();
+        },
       },
-   },
+      rating: {
+        compute(product) {
+          return product.rating.toString();
+        },
+      },
+    },
+  },
 });
